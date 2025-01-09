87005a561454b31598de272b36b4ae320ac3acd7
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2025-01-10 00:53:22 +0700

    Thêm lệnh migrate và cải tiến cấu hình kết nối cơ sở dữ liệu

--- src/config/config.js
+++ src/config/config.js
@@ -1,42 +1,41 @@
-'use-strict'
-require('dotenv').config();
+"use-strict";
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT,
+  development: {
+    username: process.env.DB_USERNAME ?? "postgres",
+    password: process.env.DB_PASSWORD ?? "password",
+    database: process.env.DB_DATABASE ?? "db_myceph",
+    host: process.env.DB_HOST ?? "localhost",
+    port: process.env.DB_PORT ?? 5432,
+    dialect: process.env.DB_DIALECT ?? "postgres",
     dialectOptions:
-    process.env.DB_SSL === 'true' ?
-      {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      } : {}
-    ,
-    "logging":false,
-    "query": {
-      "raw": true
+      process.env.DB_SSL === "true"
+        ? {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          }
+        : {},
+    logging: false,
+    query: {
+      raw: true,
     },
-    "timezone":"+07:00"
+    timezone: "+07:00",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
-  }
-}
-
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
